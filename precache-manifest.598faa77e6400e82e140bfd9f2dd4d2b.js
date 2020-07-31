e3f7e91c0d0e4825b71e1e5de1a041d339e623cd
ashashishy <ash.ashish.y@gmail.com>
2020-07-31 13:19:50 +0530

    Updates

---
+++ precache-manifest.598faa77e6400e82e140bfd9f2dd4d2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7405a51d79cf633945223d6a1033ef3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "181242241b51ffe97483",
+    "url": "/static/css/main.5dd54621.chunk.css"
+  },
+  {
+    "revision": "7edf4a20bc9e3dbac1ce",
+    "url": "/static/js/2.a53af7bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a53af7bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181242241b51ffe97483",
+    "url": "/static/js/main.cabdcfdd.chunk.js"
+  },
+  {
+    "revision": "4d9a5397c08b555d75e9",
+    "url": "/static/js/runtime-main.583fd648.js"
+  }
+]);
